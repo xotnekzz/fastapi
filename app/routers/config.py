110520cf14abd0fd7b7a36ed0ed98d450130fa89
tskim <tskim@tskimui-MacBookPro.local>
2021-04-11 19:23:53 +0900

    init bigger application

--- app/routers/config.py
+++ app/routers/config.py
@@ -11,7 +11,7 @@ router = APIRouter(
 )
 
 @router.get("/git-pull", response_class=PlainTextResponse)
-def git_pull():
+async def git_pull():
     t1 = subprocess.Popen("git --git-dir=/var/www/fastapi/.git --work-tree=/var/www/fastapi/ checkout -- /var/www/fastapi", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     t2 = subprocess.Popen("git --git-dir=/var/www/fastapi/.git --work-tree=/var/www/fastapi/ pull",stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     
