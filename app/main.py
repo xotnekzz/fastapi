ff4f2d6343d316f07a81b6f39d2b66f4cd8eb88c
tskim <tskim@tskimui-MacBookPro.local>
2021-04-11 19:35:11 +0900

    init bigger application

--- app/main.py
+++ app/main.py
@@ -5,7 +5,8 @@ from .dependencies import get_query_token, get_token_header
 from .internal import admin
 from .routers import config
 
-app = FastAPI(dependencies=[Depends(get_query_token)])
+#app = FastAPI(dependencies=[Depends(get_query_token)])
+app = FastAPI()
 
 app.include_router(config.router)
 app.include_router(
